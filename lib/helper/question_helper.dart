f756347481f367b8357a3303078fd8473b9fd36a
Alberto <alberto@mendezcabrera.com>
2018-09-26 23:07:05 +0200

    Refactor

--- lib/helper/question_helper.dart
+++ lib/helper/question_helper.dart
@@ -5,6 +5,13 @@ import 'package:trivia_flutter_demo/model/question.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as JSON;
 
+const String KEY_RESULTS = "results";
+const String KEY_CORRECT_ANSWER = "correct_answer";
+const String KEY_INCORRECT_ANSWER = "incorrect_answers";
+const String KEY_CATEGORY = "category";
+const String KEY_DIFFICULTY = "difficulty";
+const String KEY_QUESTION = "question";
+
 final String url = "https://opentdb.com/api.php?amount=1&type=multiple";
 
 Future<Question> retrieveQuestion() async {
@@ -13,13 +20,13 @@ Future<Question> retrieveQuestion() async {
     final response = await http.get(url);
     final jsonResponse = JSON.jsonDecode(response.body);
     Answer correctAnswer = new Answer(
-        text: jsonResponse['results'][0]['correct_answer'], correct: true);
+        text: jsonResponse[KEY_RESULTS][0][KEY_CORRECT_ANSWER], correct: true);
     Answer incorrectAnswer1 = new Answer(
-        text: jsonResponse['results'][0]['incorrect_answers'][0], correct: false);
+        text: jsonResponse[KEY_RESULTS][0][KEY_INCORRECT_ANSWER][0], correct: false);
     Answer incorrectAnswer2 = new Answer(
-        text: jsonResponse['results'][0]['incorrect_answers'][1], correct: false);
+        text: jsonResponse[KEY_RESULTS][0][KEY_INCORRECT_ANSWER][1], correct: false);
     Answer incorrectAnswer3 = new Answer(
-        text: jsonResponse['results'][0]['incorrect_answers'][2], correct: false);
+        text: jsonResponse[KEY_RESULTS][0][KEY_INCORRECT_ANSWER][2], correct: false);
     List<Answer> answersList = [
       correctAnswer,
       incorrectAnswer1,
--- lib/model/question.dart
+++ lib/model/question.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:meta/meta.dart';
+import 'package:trivia_flutter_demo/helper/question_helper.dart';
 import 'package:trivia_flutter_demo/model/answer.dart';
 
 class Question {
@@ -16,13 +17,13 @@ class Question {
       @required this.answersList});
 
   Question.fromJson(Map<String, dynamic> json, List<Answer> answersList)
-      : question = json['results'][0]['question'],
-        category = json['results'][0]['category'],
-        difficulty = json['results'][0]['difficulty'],
+      : question = json[KEY_RESULTS][0][KEY_QUESTION],
+        category = json[KEY_RESULTS][0][KEY_CATEGORY],
+        difficulty = json[KEY_RESULTS][0][KEY_DIFFICULTY],
         this.answersList = answersList;
 
   @override
   String toString() {
-    return 'Question{category: $category, difficulty: $difficulty, question: $question, Answers: $answersList}';
+    return 'Question{category: $category, difficulty: $difficulty\n\tquestion: $question\n\tanswers: $answersList}';
   }
 }

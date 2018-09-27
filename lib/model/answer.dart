1638d0fd14c62ce86830bd633626ddbbe752c740
Alberto <alberto@mendezcabrera.com>
2018-09-27 23:14:20 +0200

    Parsed the answers HTML code too

--- lib/model/answer.dart
+++ lib/model/answer.dart
@@ -1,10 +1,13 @@
 import 'package:meta/meta.dart';
+import 'package:html/parser.dart';
 
 class Answer{
   String text;
   bool correct;
 
-  Answer({@required this.text, @required this.correct});
+  Answer({@required text, @required this.correct}){
+    this.text = parse(text).firstChild.text;
+  }
 
   @override
   String toString() {
